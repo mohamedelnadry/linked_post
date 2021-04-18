4c6c34a5860f4857d668d62ff0e2101a8d62859a
mohamedelnadry <mohamdelnadry12345@gmail.com>
2021-04-18 02:13:31 +0200

    edit in validation

--- validators/register.validators.js
+++ validators/register.validators.js
@@ -2,8 +2,8 @@ const { check } = require('express-validator');
 
 
 module.exports = [
-    check('Fname').matches((/^[a-z ,.'-]+$/)),
-    check('Lname').matches((/^[a-z ,.'-]+$/)),
+    check('Fname').matches((/^[A-Za-z ,.'-]+$/)),
+    check('Lname').matches((/^[A-Za-z ,.'-]+$/)),
     check('Username').matches((/^[a-zA-Z0-9]+$/)),
     check('Email').isEmail(),
     check('Password').matches(/^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{8,}$/)
